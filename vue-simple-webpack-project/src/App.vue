3543967c3147f9dc75b29c9063091c51b6e098b9
Farhan Naeem <farhannaeem040@gmail.com>
2019-11-01 04:39:00 +0300

    Sample vue app

---
+++ vue-simple-webpack-project/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+        
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    var p = new Promise();
+    alert("hello");
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
